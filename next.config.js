64f91cc963540f8dde1c7dae6d1d0e92350efc4f
Ethan Muir <ethanmuir12@gmail.com>
2023-09-26 19:07:02 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
-    //output: 'export',
+    reactStrictMode: true,
+    swcMinify: true,
 }
+  
+  module.exports = nextConfig
 
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     trailingSlash: true,
+//     //output: 'export',
+// }
+
+// module.exports = nextConfig
