5fb63eaa6327d9dc8fbd163ddd70a6a85a8eaef2
Ethan Muir <ethanmuir12@gmail.com>
2023-09-26 01:42:21 -0400

    Update contact.js

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer"
 
 export default async function ContacApi(req, res) {
     const {email, phone, first, last, message} = req.body
-    
+
     console.log("got here");
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -27,7 +27,45 @@ export default async function ContacApi(req, res) {
         });
     });
 
+    // try {
+    //     const { method } = req;
+    //     switch (method) {
+    //       case "POST": {
+    //         //Do some thing
+    //         await sendMail({
+    //             from:"ethanmuir12@gmail.com",
+    //             to: "ethanmuir12@gmail.com",
+    //             replyTo: email,
+    //             subject: 'Contact form submission',
+    //             html:`
+    //             <p>Name: ${first} ${last}</p>
+    //             <p>Phone: ${phone}</p>
+    //             <p>Email: ${email}</p>
+    //             <p>Message: ${message}</p>
+    //             `
+    //             }
+    //         );
+    //         res.status(200).send("Success");
+    //         break;
+    //       }
+    //       case "GET": {
+    //         //Do some thing
+    //         res.status(200).send(req.auth_data);
+    //         break;
+    //       }
+    //       default:
+    //          res.setHeader("Allow", ["POST", "GET", "PUT", "DELETE"]);
+    //          res.status(405).end(`Method ${method} Not Allowed`);
+    //         break;
+    //     }
+    //   } catch (err) {
+    //     res.status(400).json({
+    //       error_code: "api_one",
+    //       message: err.message,
+    //     });
+    //   }
     try{
+        res.setHeader("Allow", ["POST", "GET", "PUT", "DELETE"]);
         const mail = await transporter.sendMail({
             from:"ethanmuir12@gmail.com",
             to: "ethanmuir12@gmail.com",
